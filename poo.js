77f5e3d761c1577356bf64de2fcf09bfb0f2bb1d
nilsonns.ti <nilsonns.ti@gmail>
2025-06-30 23:02:55 -0300

    Frist commit

---
+++ poo.js
@@ -0,0 +1,43 @@
+// Criando um objeto de forma literal no JS.
+
+const carroDeGabriel = {
+    // Atributo
+    modelo: "Ferrari",
+    fabricante:"Ferrari",
+    anoModelo:2035,
+    anoFabricacao: 2035,
+    // Método
+    acelerar: function(){
+        console.log("80km/s")
+    }
+}
+
+const carroDeLevi = {
+    // Atributos
+    modelo:"Ferrari",
+    fabricante: "Ferrari",
+    anoModelo: 2030,
+    anoFabricacao: 2030,
+    // método
+    acelerar: function(){
+        console.log("80Km/s")
+    }
+}
+
+// Criando um objeto com função construtora.
+
+function Carro(modelo, fabricante, anoModelo, anoFabricacao){
+    this.modelo = modelo
+    this.fabricante = fabricante
+    this.anoFabricacao = anoFabricacao
+    this.anoModelo = anoModelo
+    this.acelerar = function(){
+        console.log("80Km/s")
+    }
+}
+
+const carroDeGabrielV2 = new Carro("Ferrari", "Ferrari", 2035, 2035)
+const carroDeLeviV2 = new Carro("Ferrari", "Ferrari", 2030, 2030)
+
+console.log(carroDeGabrielV2)
+console.log(carroDeLeviV2)
