a28b6615a226bb7f38d6d74e28123f222a98f57d
Nascimento <nnasci10@ford.com>
2025-07-05 22:10:23 -0300

    herança.v1

--- funcionarios.js
+++ funcionarios.js
@@ -1,7 +1,42 @@
+// ########## Sem Herança ############################
+
 function Pessoa(nome){
     this.nome = nome
+    this.dizOi = function(){
+        console.log(this.nome + " diz Olá")
+    }
 }
 
+function Funcionario(nome, cargo, salario){
+    this.nome = nome
+    this.cargo = cargo
+    this.salario = salario
+}
+
+// ############ Herança #########################
+
+function Pessoa(nome){
+    this.nome = nome
+    this.dizOi = function(){
+        console.log(this.nome + " diz Olá")
+    }
+}
+
+function Funcionario(nome, cargo, salario){
+    this.nome = nome
+    this.cargo = cargo
+    this.salario = salario
+}
+
+
 const pessoa1 = new Pessoa("Maria")
+const funcionario1 = new Funcionario("José", "Carpinteiro", "R$ 7.000,00")
+const funcionario2 = new Funcionario("Pedro", "Pescador", 10000)
+
+// pessoa1.dizOi()
+
+funcionario1.dizAi()
 
 console.log(pessoa1)
+console.log(funcionario1, "Estância de funcionário 1")
+console.log(funcionario2, "Estância de funcionário 2")
