661f5db7cf07588768cc9d1ea8eabba9090ad53c
Nilson Nascimento <nilsonns.ti@gmail.com>
2025-09-26 10:41:14 -0300

    First commit

---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+// Function to multiply two numbers
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+// Functio to concatenate "Olá" and one variable string
+
+function greet(name: string): string {
+    return `Olá, ${name}!`;
+}
---
+++ src/exercicio_2.ts
@@ -0,0 +1,10 @@
+function summer(n1: number, n2: number): number{
+    return n1 * n2
+}
+
+console.log(summer(7,7))
+function hello(name: string): string{
+    return `Olá ${name}`
+}
+
+console.log(hello("Matheus"))
