83aec408a84e8bb13775744a827df1e070b2ebd7
Nilson Nascimento <nilsonns.ti@gmail.com>
2025-10-22 10:17:55 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Perfil from './components/Perfil';
+// import "./index.css";
+
+function App() {
+    return (
+       <div className='encapsuling-div'>
+            <Perfil />
+        </div>    
+    )
+}
+
+export default App;
---
+++ src/components/Perfil/index.jsx
@@ -0,0 +1,32 @@
+import "./index.css";
+
+const Perfil = () => {
+    const usuario = {
+        name: 'Nilson Nascimento',
+        avatarGithub: 'https://github.com/Nilson-Nascimento.png',
+    }
+    
+    return (
+        <div>
+            <h1 className="logo-spin">Desenvolvedor Full Stack Java</h1> {/* Título principal exibido em um elemento h1. Em React */}
+            <h2 >{usuario.name}</h2> {/* Nome do usuário exibido em um elemento h2. */}
+            <img className="logo-spin" src={usuario.avatarGithub} /> {/* Imagem do avatar do GitHub. Em src não é necessário as "" duplas*/ }
+            <p>Olá 👋 Eu Sou Nilson Nascimento
+🌟 Uma Jornada através do Mundo do Código
+
+Desde os primeiros dias, Java foi meu ponto de partida. Na Unifacs, mergulhei na programação orientada a objetos. Mas a jornada não parou por aí.
+
+⚙️ Capítulo 2: Explorando o Stack Tecnológico Do frontend ao backend, me tornei um mestre fullStack. Java, HTML, JavaScript e CSS se tornaram ferramentas essenciais. Construí sistemas complexos e elegantes que deixaram uma marca no mundo digital.
+
+🔍 Capítulo 3: Diversificando Minhas Habilidades A busca pelo conhecimento nunca cessou. Explorei Python, R e outras ferramentas de análise de dados, expandindo meu repertório e enriquecendo minhas habilidades como desenvolvedor.
+
+🌐 Capítulo 4: Conexão entre Frontend e Backend Entendi a importância da integração perfeita entre o frontend e o backend. Java continuou sendo a espinha dorsal, enquanto HTML, JavaScript e CSS deram vida e beleza às minhas criações.
+
+💻 Capítulo 5: Escrevendo o Futuro do Código Agora, compartilho minha experiência e conhecimento com outros entusiastas. Cada projeto fullStack é uma nova história a ser contada, uma oportunidade para criar algo extraordinário e deixar minha marca no mundo da tecnologia.
+
+Bem-vindo à minha jornada pelo mundo do código! Juntos, vamos explorar novos horizontes e escrever o futuro da tecnologia. 🚀🌐</p>
+        </div>
+    );
+}
+
+export default Perfil;
