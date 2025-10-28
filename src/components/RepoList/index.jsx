dd344498588e3a9cfc42d7356578369f0a35b9a9
Nilson Nascimento <nilsonns.ti@gmail.com>
2025-10-28 16:00:04 -0300

    reposList.v1

--- src/components/RepoList/index.jsx
+++ src/components/RepoList/index.jsx
@@ -1,13 +1,54 @@
 import { useState, useEffect } from "react";
+// import { createConnection } from "../../services/api";
 
 const ReposList = () => {
     const [repos, setRepos] = useState([])
+    const [serverUrl, setServerUrl] = useState('https://api.github.com/users/Nilson-Nascimento/repos')
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(serverUrl) // createConnection()
+            .then(response => response.json()) // 
+            .then(data => {
+                setTimeout(() => { // Simula um atraso de 3 segundos
+                    setLoading(false)
+                    console.log('Dados recebidos do servidor:')
+                    setRepos(data) // armazenar os dados no estado.
+                }, 3000)
+            })
+    }, [])
     return (
-        <ul>
-            <li>
-                Repositório 1
-            </li>
-        </ul>
+        <div className="encapsuling">
+            {loading && <p>Carregando repositórios...</p>} {/*(
+            <p>Carregando repositórios...</p>
+            ) É possível usar os parênteses para quebrar linhas*/}
+            <h1>Repositórios</h1>
+            <ol type="0" start="1" >
+                {/* Method 1 */}
+                {/* {repos.map((repo, index) => (
+                    <li key={repo.id}> {index + 1} -
+                        Nome: <a href={repo.html_url} target="_blank" rel="noreferrer">{repo.name}</a>
+                        <b>Linguagem:</b> {repo.language ? repo.language : 'N/A'}
+                    </li>
+                ))} */}
+                {/* Method 2 */}
+                {/* {repos.map(repo => (
+                    <li key={repo.id}>
+                        <b>Nome:</b> <a target="_blank" href={repo.html_url}>{repo.name}</a> <br />
+                        <b>Linguagem</b> : {repo.language ? repo.language : 'N/A'}
+                    </li>
+                ))} */}
+                {/* Method 3 - Desestrutuction */}
+                {repos.map(({id, name, html_url, language}) => (
+                    <li key={id}>
+                        <p>
+                            <b>Nome:</b> <a target="_blank" href={html_url}>{name}</a>
+                            <b>Linguagem: </b>{language ? language: 'N/A'}
+                        </p>
+                    </li>
+                ))}
+            </ol>
+        </div>
     )
 
 }
